54efedcbe36519f54911cebfd762b706b10609ee
Magojohnji <magojohnji@outlook.com>
2024-07-03 10:32:09 +0800

    D D B B A A A A B B B

---
+++ README.md
@@ -0,0 +1,4 @@
+# A Hybrid Minecraft Server
+**Mohist 1.20.1**
+
+*It just a server with serveral mods and plugins.*
--- scripts/modify-motd.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-
-var serverProp = fs.readFileSync("./server.properties", { encoding: "utf8" });
-
-serverProp = serverProp.replaceAll("motd=A Minecraft Server", "motd=" + process.argv[2]);
-
-fs.writeFileSync("./server.properties", serverProp);
